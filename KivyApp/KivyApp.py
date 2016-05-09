57471b69373861c1d9a686480f9d94fa96008376
Pranay Pradhananga <Pranay Pradhananga>
2016-05-09 21:02:19 +1000

    Add scrollable view to the rightlayout for user

--- KivyApp/KivyApp.py
+++ KivyApp/KivyApp.py
@@ -12,6 +12,7 @@ from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.spinner import Spinner
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.scrollview import ScrollView
 
 class LetLayout(BoxLayout):
     def __init__(self, **kwargs):
@@ -82,16 +83,18 @@ class MainWidget(Widget):
     pass  
 
 
-class MainLayout(BoxLayout):
+class MainLayout(GridLayout):
     def __init__(self, **kwargs):
         super(MainLayout, self).__init__(**kwargs)
+      
         self.cols = 2
         self.orientation = "vertical"
         self.padding = 10
         self.y_pos= .8
+       
 
-        self.left_layout = FloatLayout(orientation="vertical")      
-        self.add_widget(self.left_layout)
+        self.left_layout = GridLayout(cols=1,  pos=(5,-10), size_hint=(.4,1))      
+        
 
         self.spinner = Spinner( text = 'Menu', values =('New', 'Work', 'Other', 'Custom'), size_hint =(None, None), size=(200,44), pos_hint={'x':.1, 'y':.9})
         self.left_layout.add_widget(self.spinner)
@@ -105,11 +108,30 @@ class MainLayout(BoxLayout):
         self.print_button = Button(text="PRINT", size_hint =(None, None), size=(200,44),pos_hint={'x':.1, 'y':.6})
         self.left_layout.add_widget(self.print_button) 
 
-       
+        self.add_widget(self.left_layout)
+
         self.orientation = "horizontal"
-        self.right_layout = FloatLayout(padding=10, orientation ="vertical")
+
+        self.grid_layout = GridLayout(cols=1,  padding=10, spacing=10, size_hint=(None, None), width=500)
+        self.grid_layout.bind(minimum_height=self.grid_layout.setter('height'))
+        self.right_layout = ScrollView(size_hint=(None, None),size=(500, 320), pos_hint={'center_x': .5, 'center_y': .5}, do_scroll_x=False, do_scroll_y=True)
         self.add_widget(self.right_layout)
+        
        
+        self.right_layout.add_widget(self.grid_layout)
+        
+        
+
+
+        #self.right_layout = FloatLayout(pos=(200, -10) ,  size_hint=(.5,1))
+        #self.right_layout = GridLayout(cols=1, pos=(300,-10))  
+        #self.add_widget(self.right_layout)
+
+        self.bottom_layout = GridLayout( cols=1,pos=(5,-200), size_hint=(1,.4))
+        self.comment = TextInput(text='Comment' )    
+        
+        self.bottom_layout.add_widget(self.comment)
+        self.add_widget(self.bottom_layout)       
         self.new_button.bind(on_press=self.new_program)
         self.let_button.bind(on_press =self.add_let)
         self.print_button.bind(on_press = self.add_print)
@@ -123,15 +145,19 @@ class MainLayout(BoxLayout):
              self.spinner.text = 'Menu'
 
     def new_program(self,instance):
-        self.right_layout.clear_widgets()
+        self.grid_layout.clear_widgets() # clear just the widgets inside the grid layout
+        #self.right_layout.clear_widgets() # right layout is scroll view, grid layout is containd in this layout. do not clear this widget
         self.y_pos = .8
 
     def add_let(self, instance):
-        self.right_layout.add_widget(LetLayout(size_hint=(None, None),  pos_hint={'x':.1,'y':self.y_pos}))
+        #self.right_layout.add_widget(LetLayout(size_hint=(None, None),  pos_hint={'x':.2,'y':self.y_pos}))
+        self.grid_layout.add_widget(LetLayout(size_hint=(None, None),  pos_hint={'x':.2,'y':self.y_pos}))
         self.y_pos -= .1
 
     def add_print(self, instance):
-        self.right_layout.add_widget(PrintLayout( size_hint=(None, None),  pos_hint={ 'x':.1,'y':self.y_pos}))
+        #self.right_layout.add_widget(PrintLayout( size_hint=(None, None),  pos_hint={ 'x':.2,'y':self.y_pos}))
+        self.grid_layout.add_widget(PrintLayout( size_hint=(None, None),  pos_hint={ 'x':.2,'y':self.y_pos}))
+        
         self.y_pos -= .1
 
 
@@ -148,6 +174,35 @@ class MyApp(App):
         #return LetLayout()
         #return MainWidget()
         #pass
+
+        #layout = GridLayout(cols=1, padding=10, spacing=10,
+        #        size_hint=(None, None), width=500)
+
+        # when we add children to the grid layout, its size doesn't change at
+        # all. we need to ensure that the height will be the minimum required to
+        # contain all the childs. (otherwise, we'll child outside the bounding
+        # box of the childs)
+        
+        #layout.bind(minimum_height=layout.setter('height'))
+
+        # add button into that grid
+        #for i in range(30):
+        #    btn = Button(text=str(i), size=(480, 40),
+        #                 size_hint=(None, None))
+        #    layout.add_widget(btn)
+
+
+        #layout = MainLayout()
+        #layout.bind(minimum_height=layout.setter('height'))
+        ## create a scroll view, with a size < size of the grid
+        #root = ScrollView(size_hint=(None, None), size=(500, 320),
+        #        pos_hint={'center_x': .5, 'center_y': .5}, do_scroll_x=False)
+        #root.add_widget(layout)
+
+        #return root
+
+
+
         return MainLayout()
 
 if __name__ == '__main__':
