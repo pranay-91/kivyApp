ab4e7fab6e764d055744ea51a277bd5e6be710a7
pranay-91 <pranay.pradhananga@gmail.com>
2016-05-10 20:03:25 +1000

    Added output to store results. get_output method to return output and get_variable to return memory variables

--- KivyApp/Interpreter.py
+++ KivyApp/Interpreter.py
@@ -9,18 +9,26 @@ class Interpreter(object):
     def __init__(self):
         self.memory = Memory()
         self.lines = {}
+        self.output=[]
         self.op_maker = OperationFactory(self.memory)
 
     def add_line(self, number, line):  
         self.lines[int(number)] = line
 
+    def get_output(self):
+        return self.output
+
+    def get_variables(self):
+        return self.memory.get_variables()
 
     def run(self):
         line_number = sorted(self.lines.keys())
         for each_number in line_number:
             current_line = self.lines[each_number]
             op = self.op_maker.create_operation(current_line)
-            op.operate(self.memory)
+            result = op.operate(self.memory)
+            if result is not 0:
+                self.output.append(result)
 
     
 
